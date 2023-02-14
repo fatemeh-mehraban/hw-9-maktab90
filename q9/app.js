b668d59722ab3fff016f5f8fc6ce2ce14f739140
mehraban <mehraban.fatemeh73@gmail.com>
2023-02-14 13:52:34 +0330

    q-9

---
+++ q9/app.js
@@ -0,0 +1,5 @@
+let hover= document.querySelector(".hover-text-js")
+let tooltip= document.querySelector(".tooltipjs")
+hover.addEventListener("mouseover" , ()=>{
+    tooltip.classList.tagel("active")
+})
